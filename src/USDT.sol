05fc47294a26da2f8c7ee046ad68a2c40d4dff53
OxCrispy <crispy@figarolabs.com>
2024-03-17 16:54:39 -0400

    Add vault, update close position & liquidate logic

---
+++ src/USDT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.20;
+
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// https://github.com/matter-labs/local-setup/blob/main/rich-wallets.json
+contract USDT is ERC20 {
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+        _mint(_msgSender(), 100_000_000_000 ether);
+    }
+}
