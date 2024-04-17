b130ffa6d6cf36d5bc6df6f8d8457f3b2b80ed16
OxCrispy <157279915+OxCrispy@users.noreply.github.com>
2024-04-16 20:36:01 -0400

    Fix admin transfer

--- src/PPFX.sol
+++ src/PPFX.sol
@@ -419,10 +419,10 @@ contract PPFX is IPPFX, Context {
      *     
      */
      function acceptAdmin() external {
-        require(_msgSender() == pendingAdmin, "Caller not pendingAdmin");
         require(pendingAdmin != address(0));
+        require(_msgSender() == pendingAdmin, "Caller not pendingAdmin");
         _updateAdmin(pendingAdmin);
-        pendingAdmin == address(0);
+        pendingAdmin = address(0);
      }
 
 
@@ -439,7 +439,6 @@ contract PPFX is IPPFX, Context {
      * - `adminAddr` cannot be the zero address.
      */
     function transferAdmin(address adminAddr) external onlyAdmin() {
-        require(adminAddr != address(0), "Admin address can not be zero");
         _transferAdmin(adminAddr);
     }
 
